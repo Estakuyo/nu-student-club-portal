a4c50ca0526ac7eab04839b6b3136d247a741438
dedS3C5 <159559448+Estakuyo@users.noreply.github.com>
2025-05-04 05:26:04 +0800

    Started editing the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
+import './styles/Home.css'
+import NULogo from '../assets/NU Logo.png'
+
+// Components and Pages
+import Navigation from '../components/Navigation'
+
 export default function Home() {
     return (
         <div className="home">
-            <h1>Hello World</h1>
+            <img src={NULogo} alt='NU Logo' />
+            <h1>NU Student Clubs</h1>
         </div>
     )
 }
